a0328b50aedeb101e144378b47477c0f3d0a9d11
ClaseFede <d49557059@alumnos.itr.edu.ar>
2026-03-12 11:21:42 -0300

    hola

---
+++ esElPrimero.py
@@ -0,0 +1,26 @@
+import os
+os.system("clear")
+
+numero =input("¿Quieres iterar un numero? (SI/NO) ")
+
+if numero == "SI" or numero == "si" or numero == "Si" or numero == "sí":
+    numeroUsado = float(input("Introduce un numero: "))
+    while numero == "SI" or numero == "si" or numero == "Si" or numero == "sí":
+        incrementar = input("¿Para incrementar el numero prseiona 1, para disminuir apleta 2? ")
+        if incrementar == "1":
+            numeroUsado += 1
+            print("El numero es: ", numeroUsado)
+            pregunta = input("¿Quieres volver a iterar el numero? (SI/NO) ")
+            numero = pregunta
+            cambiar = input(f"¿Quieres cambiar el numero? (SI/NO) ")
+            if cambiar == "SI" or cambiar == "si" or cambiar == "Si" or cambiar == "sí":
+                numeroUsado = float(input("Introduce un numero: "))
+        elif incrementar == "2":
+            numeroUsado -= 1
+            print("El numero es: ", numeroUsado)
+            pregunta = input("¿Quieres volver a iterar el numero? (SI/NO) ")
+            numero = pregunta
+            cambiar = input(f"¿Quieres cambiar el numero? (SI/NO) ")
+            if cambiar == "SI" or cambiar == "si" or cambiar == "Si" or cambiar == "sí":
+                numeroUsado = float(input("Introduce un numero: "))
+
