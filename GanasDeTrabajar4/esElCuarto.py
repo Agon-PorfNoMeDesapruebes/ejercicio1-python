1ac3730c5b4039eb114ecaefaa27229756162881
ClaseFede <d49557059@alumnos.itr.edu.ar>
2026-04-03 20:38:26 -0300

    Añadido raro de la cuarta actividad, punto 3

--- GanasDeTrabajar4/esElCuarto.py
+++ GanasDeTrabajar4/esElCuarto.py
@@ -1,19 +1,52 @@
 """
-
-def agregar_tarea(tareas, tarea, fecha_limite, prioridad):
-    nueva_tarea = {"Tarea": tarea, "Fecha limite": fecha_limite, "Prioridad": prioridad}
+"""
+def agregar_tarea(tareas, tarea, fecha_limite, prioridad, completada = False):
+    nueva_tarea = {"Tarea": tarea, "Fecha limite": fecha_limite, "Prioridad": prioridad, "Estado": completada}
     tareas.append(nueva_tarea)
     print("Tarea agregada exitosamente.")
 
+def marcar_completada(tareas):
+    if not tareas:
+        print("No hay tareas para marcar.")
+        return
+
+    print("Ingresa cual tarea quieres marcar como completada: ")
+    numero = input()
+
+    if numero.isdigit():
+        indice = int(numero) - 1
+
+        if indice >= 0 and indice < len(tareas):
+            tareas[indice]["Estado"] = True
+            print("Tarea marcada como completada.")
+        else:
+            print("Número inválido.")
+    else:
+        print("Debes ingresar un número.")
 
 def mostrar_tareas(tareas):
     if not tareas:
         print("No hay tareas pendientes.")
     else:
-        for i, tarea in enumerate(tareas, 1):
-            print(f"\nTarea {i}:")
-            for clave, valor in tarea.items():
-                print(f"{clave}: {valor}")
+        cuestion = input("Quieres ver solamente las completas (1), solamente las que no lo estan (2) o todas (3): ")
+        if cuestion == "1":
+            for i, tarea in enumerate(tareas, 1):
+                if tarea["Estado"] == True:
+                    print(f"\nTarea {i}:")
+                    for clave, valor in tarea.items():
+                        print(f"{clave}: {valor}")
+        if cuestion == "2":
+            for i, tarea in enumerate(tareas, 1):
+                if tarea["Estado"] == False:
+                    print(f"\nTarea {i}:")
+                    for clave, valor in tarea.items():
+                        print(f"{clave}: {valor}")
+        if cuestion == "3":
+            for i, tarea in enumerate(tareas, 1):
+                print(f"\nTarea {i}:")
+                for clave, valor in tarea.items():
+                    print(f"{clave}: {valor}")
+        
 
 
 if __name__ == "__main__":
@@ -22,7 +55,8 @@ if __name__ == "__main__":
     while True:
         print("\n1. Agregar tarea")
         print("2. Mostrar tareas")
-        print("3. Salir")
+        print("3. Marcar completada")
+        print("4. Salir")
 
         opcion = input("Seleccione una opción: ")
 
@@ -36,10 +70,13 @@ if __name__ == "__main__":
             mostrar_tareas(lista_tareas)
 
         elif opcion == "3":
+            marcar_completada(lista_tareas)
+
+        elif opcion == "4":
             break
 
         else:
             print("Opción no válida. Intente nuevamente.")
 
-"""
+
 
