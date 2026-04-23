8f9ae254201964949266773efde62f3c2bbcf25c
ClaseFede <d49557059@alumnos.itr.edu.ar>
2026-04-23 15:04:56 -0300

    Funciona Código

--- Flask/app.py
+++ Flask/app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template
 
-#Inicio de la aplicación(Todo va adentro)
+# Inicio de la aplicación (todo va adentro)
 app = Flask(__name__)
 
-app.run(debug = True)
-
 @app.route('/')
+def index():
+    return render_template('base.html')
+
 @app.route('/contacto')
-
-def principal():
-    return render_template('index.html')
-
 def contacto():
-    return "Contacto"
+    return render_template('contacto.html')
 
-#Termina la aplicación
+@app.route('/lenguajes')
+def lenguajes():
+    return render_template('lenguajes.html')
+
+# Termina la aplicación
 if __name__ == '__main__':
-    app.run(debug = True, port = 3500)
+    app.run(debug=True, port=3500)
